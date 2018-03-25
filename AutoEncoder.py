a77932aca3ca04bb61a1ffdfaa98ece567a3ebce
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-03-26 01:25:22 +0430

    Add AE Files

---
+++ AutoEncoder.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import tensorflow as tf
+from keras.layers import Dense, Input
+from keras.models import Model
+from keras.datasets import mnist
+
+"""
+    Created by Mohsen Naghipourfar on 3/26/18.
+    Email : mn7697np@gmail.com
+    Website: http://ce.sharif.edu/~naghipourfar
+"""
+
+
